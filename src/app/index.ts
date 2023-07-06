668ff6a68b7f658d2850990b42cbbf033386aba1
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2023-07-06 20:43:22 +0900

    add tests

---
+++ src/app/index.ts
@@ -0,0 +1,7 @@
+import { add } from "../lib";
+
+main();
+
+function main() {
+  console.log("Addition of 1 and 2 is", add(1, 2));
+}
---
+++ src/tests/index.ts
@@ -0,0 +1,14 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+import { add } from "../lib";
+
+describe("add", { skip: process.env["TEST"] }, () => {
+  it("should add two numbers", () => {
+    assert.strictEqual(add(1, 2), 3);
+  });
+
+  it("does not minus two numbers", () => {
+    assert.notStrictEqual(add(1, 2), -1);
+  });
+});
